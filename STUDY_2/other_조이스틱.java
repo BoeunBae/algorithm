f1c270836d7163fac27c5bb104d9a471b4fd59f1
BoeunBae <45895847+BoeunBae@users.noreply.github.com>
2020-01-27 23:22:55 +0900

    2주차 조이스틱 다른 방법으로 풀이_미완성

--- STUDY_2/other_조이스틱.java
+++ STUDY_2/other_조이스틱.java
@@ -1,5 +1,15 @@
 package STUDY_2;
 
 public class other_조이스틱 {
-
+	public static int solution(String name) {
+	    int answer = 0;
+	    for(int i = 0; i<name.length(); i++) {
+	    	char c = name.charAt(i);
+			if(c!='A' && c >= 'N') answer += 'Z'-c+1;
+			else if(c!='A' && c < 'N') answer += c-'A';
+		}
+	    
+	    
+	    return answer;
+	}
 }
