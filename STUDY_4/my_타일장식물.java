2f50b99c59b5ded67f0b2ecc1aa4d263174db624
BoeunBae <45895847+BoeunBae@users.noreply.github.com>
2020-02-06 11:43:26 +0900

    4주차 과제 - 타일장식물 피드백

--- STUDY_4/my_타일장식물.java
+++ STUDY_4/my_타일장식물.java
@@ -1,7 +1,10 @@
 package STUDY_4;
 
 //시간초과 효율성 0점
+//메모이제이션 활용하기
 public class my_타일장식물 {
+	long[] m = new long[81];
+	
     public long solution(int N) {
         long answer = 0;
         long max_length = getLength(N);
@@ -9,9 +12,10 @@ public class my_타일장식물 {
         answer = 2*(max_length + before_length) + 2*max_length;
         return answer;
     }
-    public int getLength(int N){
+    public long getLength(int N){
         if(N==1) return 1;
         if(N==2) return 1;
-        return getLength(N-1)+getLength(N-2);
+        if(m[N]!=0) return m[N];//이미 계산된 값일 경우 바로 반환
+        return m[N]=getLength(N-1)+getLength(N-2);
     }
 }
